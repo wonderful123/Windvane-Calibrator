41b2651a3309bae63cbe6af682e3bcb110884b04
Josh Jenkins <clearharry@gmail.com>
2025-07-03 12:06:03 +1000

    _max defined in Arduino.h, Rename to _maxEntries

--- lib/WindVane/Diagnostics/BufferedDiagnostics.h
+++ lib/WindVane/Diagnostics/BufferedDiagnostics.h
@@ -10,7 +10,7 @@
 class BufferedDiagnostics : public IDiagnostics, public IBufferedDiagnostics {
 public:
     explicit BufferedDiagnostics(size_t maxEntries = 10)
-        : _max(maxEntries) {}
+        : _maxEntries(maxEntries) {}
 
     void info(const char* msg) override {
 #ifdef ARDUINO
@@ -31,11 +31,11 @@ public:
     void clear() override { _messages.clear(); }
 
 private:
-    size_t _max;
+    size_t _maxEntries;
     std::deque<std::string> _messages;
 
     void push(const std::string& m) {
-        if (_messages.size() >= _max)
+        if (_messages.size() >= _maxEntries)
             _messages.pop_front();
         _messages.push_back(m);
     }
